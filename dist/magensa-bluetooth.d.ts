f3350852003d349f612341a83ea6fe9f10b477ac
Luke Turpin <Luke.Turpin@magtek.com>
2020-05-02 08:10:31 -0700

    initialize new dev branch

--- dist/magensa-bluetooth.d.ts
+++ dist/magensa-bluetooth.d.ts
@@ -1,4 +1,20 @@
+interface DeviceObject {
+    id: string,
+    name: string,
+    deviceType: string,
+    deviceInterface: {
+        openDevice(): Promise<any>;
+        startTransaction(emvOptions?: any): Promise<any>;
+        cancelTransaction(): Promise<any>;
+        sendCommand(writeCommand: Array<number>): Promise<any>;
+        clearSession(): Promise<any>;
+        closeDevice(): Promise<any>;
+        deviceInfo(): Promise<any>;
+        requestCardSwipe(swipeOptions?: any): Promise<any>;
+        isDeviceOpen(): boolean;
+    }
+}
+
 declare module "magensa-bluetooth" {
-    function scanForDevices(callBacks: any, deviceName: string) : any;
-    export = scanForDevices
+    export function scanForDevices(callBacks: any, deviceName?: string) : Promise<DeviceObject>;
 }
